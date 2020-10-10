de2de9dffa5c3004b6a15c5e133375eaa521e298
XX <fallwall19@gmail.com>
2020-10-09 18:22:50 -0700

    one picture view shuffle function and icon in place

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import OnePic from './pages/OnePic';
 import AllPics from './pages/AllPics';
-import CarouselSection from './components/CarouselSection';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <CarouselSection /> */}
-      <Route path="/" exact render={() => <OnePic />} />
+      <Route path="/" exact render={() => <OnePic
+   
+      />} />
       <Route path="/all" render={() => <AllPics />} />
     </div>
   );
--- src/pages/OnePic.jsx
+++ src/pages/OnePic.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState} from 'react';
+import Img from '../components/Img';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import MoreIcon from '@material-ui/icons/More';
+import data from '../data/images.json';
+
+const current = Math.floor(Math.random() * data.images.length);
 
 export default function OnePic() {
+  const [current, setCurrent] = useState(0);
   return (
-    <div>
-      
+    <div className="one-pic">
+      <Img>{data.images[current]}</Img>
+      <div className="icon">
+        <ShuffleIcon
+          fontSize="large"
+          onClick={()=>setCurrent(Math.floor(Math.random() * data.images.length))}
+        />
+        <MoreIcon fontSize="large" />
+      </div>
     </div>
+
   )
 }
