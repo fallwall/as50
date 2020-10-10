4e61303ef1cea383a4d9b8cf513dbccb16fed007
XX <fallwall19@gmail.com>
2020-10-09 17:29:09 -0700

    all pictures view + caption info

--- src/pages/AllPics.jsx
+++ src/pages/AllPics.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import data from '../data/images.json';
 
 const tileData = data.images;
@@ -20,39 +23,28 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-/**
- * The example data is structured as follows:
- *
- * import image from 'path/to/image.jpg';
- * [etc...]
- *
- * const tileData = [
- *   {
- *     img: image,
- *     title: 'Image',
- *     author: 'author',
- *     cols: 2,
- *   },
- *   {
- *     [etc...]
- *   },
- * ];
- */
-
-
 export default function AllPics() {
   const classes = useStyles();
   return (
     <div>
       <div className={classes.root}>
-      <GridList cellHeight={160} className={classes.gridList} cols={3}>
-        {tileData.map((tile) => (
-          <GridListTile key={tile.id} cols={tile.cols || 1}>
-            <img src={tile.url} alt={tile.caption} />
-          </GridListTile>
-        ))}
-      </GridList>
-    </div>
+        <GridList cellHeight={160} className={classes.gridList} cols={3}>
+          {tileData.map((tile) => (
+            <GridListTile key={tile.id} cols={tile.cols || 1}>
+              <img src={tile.url} alt={tile.caption} />
+              <GridListTileBar
+                title={tile.id + 1}
+                subtitle={<span>{tile.caption}</span>}
+                actionIcon={
+                  <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
+                    <FavoriteBorderIcon />
+                  </IconButton>
+                }
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
     </div>
   )
 }
