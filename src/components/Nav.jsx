985613d3d9d9ef6d13dd65a80dc168d0bb1771d0
XX <fallwall19@gmail.com>
2020-10-13 16:08:41 -0700

    nav flexbox

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import as from './arishack.svg';
+import DehazeIcon from '@material-ui/icons/Dehaze';
 import './Nav.css';
 
 export default function Nav() {
   return (
     <div className="nav">
-      <img src={as} className="logo" alt="logo" />
+      <div className="nav-left">
+        <img src={as} className="logo" alt="logo" />
+      </div>
+      <div className="nav-right">
+        <DehazeIcon fontSize="large" />
+      </div>
     </div>
   )
 }
