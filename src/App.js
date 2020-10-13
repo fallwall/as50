f68108b3958e4190d76b0b3f9d321a88046c7505
XX <fallwall19@gmail.com>
2020-10-13 15:21:37 -0700

    nav logo in place

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import OnePic from './pages/OnePic';
 import AllPics from './pages/AllPics';
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Route path="/" exact render={() => <OnePic
-   
+
       />} />
       <Route path="/all" render={() => <AllPics />} />
     </div>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import as from './arishack.svg';
+import './Nav.css';
+
+export default function Nav() {
+  return (
+    <div className="nav">
+      <img src={as} className="logo" alt="logo" />
+    </div>
+  )
+}
