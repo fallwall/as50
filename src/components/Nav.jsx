30a49f522b94ab8f74b8d6183cd2c55fef16cf1f
XX <fallwall19@gmail.com>
2020-10-14 11:46:41 -0700

    menu pop up

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import as from './arishack.svg';
 import DehazeIcon from '@material-ui/icons/Dehaze';
 import './Nav.css';
 
 export default function Nav() {
+  const [overlay, setOverlay] = useState(false);
   return (
-    <div className="nav">
-      <div className="nav-left">
-        <img src={as} className="logo" alt="logo" />
+    <>
+      <div className="nav">
+        <div className="nav-left">
+          <img src={as} className="logo" alt="logo" />
+        </div>
+        <div className="nav-right">
+          <DehazeIcon
+            fontSize="large"
+            className="closebtn"
+            onClick={() => setOverlay(!overlay)}
+          />
+        </div>
       </div>
-      <div className="nav-right">
-        <DehazeIcon fontSize="large" />
+      <div className="overlay" id={overlay ? "myNav" : "myNavHidden"}>
+        <div className="overlay-content">
+          <a href="#">About</a>
+          <a href="#">All</a>
+          <a href="#">Contact</a>
+        </div>
       </div>
-    </div>
+
+    </>
   )
 }
