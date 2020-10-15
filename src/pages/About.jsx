49b550aedcadd01ce5184dcd4ce8dd0b13daa4b0
XX <fallwall19@gmail.com>
2020-10-15 14:48:30 -0700

    about page svg

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,18 @@
-import React from 'react'; 
+import React from 'react';
 import hbd from '../components/arishackhbd.svg';
+import about from '../components/arishackabout.svg';
+import about2 from '../components/arishackabout2.svg';
+import about3 from '../components/arishackabout3.svg';
+
 import './About.css';
 
 export default function About() {
   return (
     <div className="about">
-      <img src={hbd} className="hbd" alt="happy birthday"/>
+      <img src={about} className="hbd" alt="happy birthday" />
+      <img src={about2} className="hbd" alt="happy birthday" />
+      <img src={about3} className="hbd" alt="happy birthday" />
+      <img src={hbd} className="hbd" alt="happy birthday" />
     </div>
   )
 }
