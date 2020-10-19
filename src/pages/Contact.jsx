fcaa9858cc1a6c6c12d0cc7b876262aebd8f3c23
XX <fallwall19@gmail.com>
2020-10-19 02:58:49 -0700

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import copyright from '../components/arishackcr.svg';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import './Contact.css';
 
 export default function Contact() {
   return (
     <div className="contact">
+      <a href="https://instagram.com/afismack66">
+      <InstagramIcon fontSize="large" /> 
+        </a>
       <img
         style={{ width:"40%" }}
         src={copyright}
